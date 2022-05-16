4e6e7bfd6a0ec5f2c6e0f004b6532d129e18e78b
rbqhr <rbqhr5452@naver.com>
2022-05-16 23:58:28 +0900

    0516

---
+++ dreamcode/scope/scope.js
@@ -0,0 +1,11 @@
+//코드블럭:{},if(){},for(){},function(){}
+{
+    const a='a';
+    console.log(a);
+}
+const b='b';
+
+function print(){
+    const message='Hello World';
+    console.log(message);
+}
