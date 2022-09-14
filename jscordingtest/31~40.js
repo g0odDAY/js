f47a307b667657ffdc5136a8ae08b34c2c24d5e1
goodDay <rbqhr5452@naver.com>
2022-09-14 15:33:34 +0900

    perf: 0914 1533
    NoteBook

--- jscordingtest/31~40.js
+++ jscordingtest/31~40.js
@@ -44,11 +44,38 @@ gugudan(9);
 
 var arr11 = ['원범','원범','혜원','혜원','혜원','혜원','유진','유진'];
 const vote = arr11.reduce((acc,cur)=>{
-        console.log(acc,cur);
         acc[cur] = (acc[cur] || 0)+1;
         return acc;
         },{});
-
 console.log(vote)
+let winner = Object.keys(vote).reduce((a,b)=>{
+    return vote[a] > vote[b] ? a:b;
+})
+console.log(`${winner}${vote[winner]}`);
 
+//38
 
+var arr37 = [97,86,75,66,55,97,85,97,97,95];
+console.log(arr37.reduce((acc,cur)=>{
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+},{}));
+
+// 강사님 코드
+let n = 5;
+let scores = [100, 88, 76, 88, 76];
+
+console.log(solution(n, scores));
+
+function solution(n, scores) {
+    let answers = Array.from({ length: n }, () => 1);
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (scores[j] > scores[i]) {
+                answers[i]++;
+            }
+        }
+    }
+    return answers;
+}
