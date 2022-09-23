296c9d62b942ce00366f84ca188e39b108945ff5
goodDay <rbqhr5452@naver.com>
2022-09-23 16:28:18 +0900

    perf: 0923 1650
    NoteBook

--- jscordingtest/31~40.js
+++ jscordingtest/31~40.js
@@ -79,3 +79,21 @@ function solution(n, scores) {
     }
     return answers;
 }
+const regex = /\d{3}-\d{3}-\d{4}/;
+console.log(regex.test('010-123-4567'));
+console.log(regex.test('010-1223-4567'));
+console.log(regex.test('010-d23-4567'));
+
+const text = "안녕하세요 제 번호는 010-123-4567이고, 010-123-4567입니다.";
+console.log(text.replace(regex, '111-111-1111'));
+console.log(text.match(regex));
+
+const text1 = '대나무 빨대 a급 제품은 10개 남아있습니다. 010-1234-5678로 Call Me~!';
+const regex1 = /[^0-9]/g;
+console.log(regex1.test(text1));
+console.log(text1.match(regex1));
+
+const tel = '01039575452';
+const telregex = /\d(?=\d{4})/g;
+console.log(tel.match(telregex, tel));
+
