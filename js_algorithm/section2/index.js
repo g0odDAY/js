ba866677a43ca8ed38a1a130ff7b04b7c3f9632f
rbqhr <rbqhr5452@naver.com>
2023-06-12 14:50:36 +0900

    perf: desktop 20230415 css

--- js_algorithm/section2/index.js
+++ js_algorithm/section2/index.js
@@ -101,3 +101,53 @@ const sameFrequency = (number1,number2)=>{
 }
 console.log(sameFrequency(233,223));
 console.log(sameFrequency(232223,222233));
+console.clear();
+const areThereDuplicates = (...args)=>{
+
+    const filterArr = new Set(args);
+    return !(args.length === filterArr.size);
+}
+console.log(areThereDuplicates(1,2,3,4,4))
+console.clear();
+const averagePair= (arr,avg)=>{
+    //1.배열의 길이가 0이면 return false
+    if(arr.length === 0) return false;
+    //2.배열의 첫번째 포인터와 마지막 포인터를 정의
+    let start = 0;
+    let end = arr.length - 1;
+
+    //3.첫번째 포인터와 마지막 포인터의 합이 avg랑 같으면 true
+    while(start<end){
+        const arryAvg = (arr[start]+arr[end])/2;
+        if(arryAvg === avg){
+            return true;
+        }else if(arryAvg < avg){
+            start++;
+        }else{
+            end--;
+        }
+    }
+    return false;
+}
+console.log(averagePair([1,2,3],2.5));
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8));
+console.log(averagePair([-1,0,3,4,5,6], 4.1));
+console.log(averagePair([], 4));
+console.clear();
+const isSubsequence = (str1,str2)=>{
+    let i = 0;
+    let j=0;
+    //str1이 공백이라면 true
+    if(!str1) return true;
+    while(j<str2.length){
+        if(str2[j] === str1[i]) i++;
+        if(i===str1.length) return true;
+        j++;
+    }
+    return false;
+
+}
+console.log(isSubsequence('abc', 'acb')); // false
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
