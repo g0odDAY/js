f38c18415857b98f505d09b3b32e2f6f030440f9
rbqhr <rbqhr5452@naver.com>
2023-06-30 09:39:18 +0900

    Queue enqueue,dequeue 단일 연결리스트로 구현

---
+++ js_algorithm/Queue.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        const newNode = new Node(val);
+        if(this.size === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+        return this;
+    }
+    dequeue(){
+        if(this.size === 0) return null;
+        const removedNode = this.head;
+        if(this.size === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = removedNode.next;
+            removedNode.next = null;
+        }
+        this.size--;
+        return removedNode;
+    }
+}
+const queue = new Queue();
+queue.enqueue('dd')
+queue.enqueue('zz')
+queue.enqueue('qq')
+queue.enqueue('ll')
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue);
