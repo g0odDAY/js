07f7d2cb0a2b189c2bc2564dcc3bd7907e9396b2
rbqhr <rbqhr5452@naver.com>
2023-07-06 01:03:38 +0900

    BFS 이진 검색 트리를 활용해서 구현. DFS 재쉬함수를 사용해서 pre,in,post order구현

---
+++ js_algorithm/Depth_first_Search.js
@@ -0,0 +1,110 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+class Binary_Search_Tree{
+    constructor() {
+        this.root = null;
+    }
+    insert(val){
+        const newNode = new Node(val);
+        if(!this.root){
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while(true){
+            if(val === current.val) return undefined;
+            if(current.val > val){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }else{
+                    current = current.left;
+                }
+            }else if(current.val < val){
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }else{
+                    current = current.right;
+                }
+            }
+        }
+    }
+    find(val){
+        if(this.root === null) return false;
+        let current = this.root;
+
+        while(current){
+            if(val < current.val){
+                current = current.left;
+            }else if(val > current.val){
+                current = current.right;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    BFS(){
+        let result = [];
+        let queue = [];
+        let node = this.root;
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            result.push(node.val);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return result;
+    }
+    DFSPreOrder(){
+        let result = [];
+        let current = this.root;
+        const traverse= (node)=>{
+            result.push(node.val);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return result;
+    }
+    DFSPostOrder(){
+        let result = [];
+        let current = this.root;
+        const traverse = (node)=>{
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            result.push(node.val);
+        }
+        traverse(current);
+        return result;
+    }
+    DFSInOrder(){
+        let result = [];
+        let current = this.root;
+        const traverse = (node)=>{
+            if(node.left) traverse(node.left);
+            result.push(node.val);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return result;
+    }
+
+}
+const list = new Binary_Search_Tree();
+list.insert(10);
+list.insert(6);
+list.insert(3);
+list.insert(8);
+list.insert(15);
+list.insert(20);
+console.log(list.DFSPreOrder());
+console.log(list.DFSPostOrder());
+console.log(list.DFSInOrder());
